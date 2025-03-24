95a17906766be6640407aff5813f14bddacdac36
badre <badre.zeggaf@intrasoft-intl.com>
2025-03-24 12:11:13 +0100

    add logs

--- IGTokenService.cs
+++ IGTokenService.cs
@@ -1,13 +1,20 @@
 public class IGTokenService
 {
 
+    private readonly ILogger<IGTokenService> _logger;
     private readonly string _instanceId = Guid.NewGuid().ToString();
     private string? _token;
     private DateTime _expiresAt;
 
+    public IGTokenService(ILogger<IGTokenService> logger)
+    {
+        _logger = logger;
+    }
+
     public async Task<string> GetTokenAsync()
     {
-        Console.WriteLine($"[IGTokenService] Instance ID: {_instanceId}");
+         _logger.LogInformation($"[IGTokenService] Instance ID: {_instanceId}");
+         
         if (_token == null)
         {
             _token = await FetchTokenFromIGAsync();
